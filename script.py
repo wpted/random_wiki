1687ee6dbe5e031ba5b33e35c9c2d6da899b5923
wpted <silverhandsome@gmail.com>
2022-05-01 23:00:01 +0800

    Add assertion in get_content

--- script.py
+++ script.py
@@ -3,6 +3,10 @@ from bs4 import BeautifulSoup
 
 
 def random_wiki_url():
+    """
+    Returns a random wikipedia page url.
+    """
+
     wiki_url = "https://en.wikipedia.org/w/api.php"
     payload = {
         "action": "query",
@@ -22,6 +26,12 @@ def random_wiki_url():
 
 
 def get_content(url):
+    """
+    Parse an input url and return the text.
+    URL should be valid.
+    """
+    assert type(url) is str, "Input Type error"  # Input should be strings
+
     result = requests.get(url=url)
     result.raise_for_status()
     content = result.text
@@ -34,8 +44,9 @@ def main():
 
     url = random_wiki_url()
     print(f"{url=}")
-    content = get_content(url)
-    print(content)
+    content = get_content("https://exampledomain.com")
+    # print(content)
+    # content = get_content("123")
 
 
 if __name__ == "__main__":
